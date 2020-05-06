41435f0ac4bec5a51c89b514b03ec60092f41a92
Aradhana <aradhana.singh@team.telstra.com>
2020-05-06 17:45:36 +0530

    rail poc react-redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { simpleAction } from './actions/simpleAction';
 
-function App() {
+const mapStateToProps = state => ({
+  ...state
+ })
+
+ const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
+class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+   }
+ render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+    <button onClick={this.simpleAction}>Test redux action</button>
+    <p className="App-intro">
+     To get started, edit <code>src/App.js</code> and save to reload
+    </p>
+    <pre>
+ {
+  JSON.stringify(this.props)
+ }
+</pre>
+   </div>
   );
+ }
 }
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
